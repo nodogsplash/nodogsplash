f7cc89158abd9cc6c7238c601abcfc9757788b3d
Marek Vavrecan <vavrecan@gmail.com>
2016-03-03 15:07:35 +0100

    allow auth by token

--- src/ndsctl_thread.c
+++ src/ndsctl_thread.c
@@ -286,6 +286,7 @@ ndsctl_auth(int fd, char *arg)
 	/* We get the client or return... */
 	if ((client = client_list_find_by_ip(arg)) != NULL);
 	else if ((client = client_list_find_by_mac(arg)) != NULL);
+	else if ((client = client_list_find_by_token(arg)) != NULL);
 	else {
 		debug(LOG_DEBUG, "Client not found.");
 		UNLOCK_CLIENT_LIST();
