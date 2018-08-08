6d066537c272550ff6f82237d4362a8f01658cbd
Moritz Warning <moritzwarning@web.de>
2018-08-08 21:08:44 +0200

    valdiate some more settings

--- src/conf.c
+++ src/conf.c
@@ -717,7 +717,7 @@ config_read(const char *filename)
 			}
 			break;
 		case oMaxClients:
-			if (sscanf(p1, "%d", &config.maxclients) < 1) {
+			if (sscanf(p1, "%d", &config.maxclients) < 1 || config.maxclients < 1) {
 				debug(LOG_ERR, "Bad arg %s to option %s on line %d in %s", p1, s, linenum, filename);
 				debug(LOG_ERR, "Exiting...");
 				exit(-1);
@@ -838,21 +838,21 @@ config_read(const char *filename)
 			}
 			break;
 		case oDownloadLimit:
-			if (sscanf(p1, "%d", &config.download_limit) < 1) {
+			if (sscanf(p1, "%d", &config.download_limit) < 1 || config.download_limit < 0) {
 				debug(LOG_ERR, "Bad arg %s to option %s on line %d in %s", p1, s, linenum, filename);
 				debug(LOG_ERR, "Exiting...");
 				exit(-1);
 			}
 			break;
 		case oUploadLimit:
-			if (sscanf(p1, "%d", &config.upload_limit) < 1) {
+			if (sscanf(p1, "%d", &config.upload_limit) < 1 || config.upload_limit < 0) {
 				debug(LOG_ERR, "Bad arg %s to option %s on line %d in %s", p1, s, linenum, filename);
 				debug(LOG_ERR, "Exiting...");
 				exit(-1);
 			}
 			break;
 		case oUploadIFB:
-			if(sscanf(p1, "%d", &config.upload_ifb) < 1) {
+			if(sscanf(p1, "%d", &config.upload_ifb) < 1 || config.upload_ifb < 0) {
 				debug(LOG_ERR, "Bad arg %s to option %s on line %d in %s", p1, s, linenum, filename);
 				debug(LOG_ERR, "Exiting...");
 				exit(-1);
