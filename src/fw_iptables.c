841747dd720336a97d5887261f18588b2e378fc2
jjm2473 <jjm2473@gmail.com>
2018-11-18 18:23:31 +0800

    return to default NAT table.

--- src/fw_iptables.c
+++ src/fw_iptables.c
@@ -508,7 +508,7 @@ iptables_fw_init(void)
 	// CHAIN_OUTGOING, packets for tcp port 80, redirect to gw_port on primary address for the iface
 	rc |= iptables_do_command("-t nat -A " CHAIN_OUTGOING " -p tcp --dport 80 -j DNAT --to-destination %s:%d", gw_address, gw_port);
 	// CHAIN_OUTGOING, other packets ACCEPT
-	rc |= iptables_do_command("-t nat -A " CHAIN_OUTGOING " -j ACCEPT");
+	rc |= iptables_do_command("-t nat -A " CHAIN_OUTGOING " -j RETURN");
 
 	/*
 	 * End of nat table chains and rules
