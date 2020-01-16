c221de18d4b4d97260a70b48a9895173bba854ee
Fran Hrzenjak <fran.hrzenjak@gmail.com>
2020-01-16 23:08:26 +0100

    added .bash scripts

---
+++ forward_authentication_service/libs/get_client_token.bash
@@ -0,0 +1,54 @@
+#!/bin/bash
+#Copyright (C) The Nodogsplash Contributors 2004-2020
+#Copyright (C) Blue Wave Projects and Services 2015-2019
+#This software is released under the GNU GPL license.
+
+# ip address of client is passed as a command line argument
+clientip=$1
+
+# exit if ip not passed
+
+if [  "$(echo "$clientip" | awk -F '.' '{print NF}')" != 4 ]; then
+	echo "
+  Usage: get_client_token.sh [clientip]
+
+  Returns: [client token]
+
+  Where:
+    [client token] is the unique client token string.
+"
+	exit 1
+fi
+
+
+wait_for_ndsctl () {
+	local timeout=3
+
+	for i in $(seq $timeout); do
+
+		if [ ! -f "/tmp/ndsctl.lock" ]; then
+			break
+		fi
+
+		sleep 1
+
+		if [ "$i" == "$timeout" ] ; then
+			pid=$(pgrep get_client_token | awk 'NR==2 {print $1}')
+			echo "ndsctl is busy or locked" | logger -p "daemon.warn" -s -t "NDS-Library[$pid]"
+			exit 1
+		fi
+
+	done
+}
+
+wait_for_ndsctl
+client_token=$(ndsctl json "$clientip" | awk -F '"' '$2=="token"{printf $4}')
+
+if [ -z "$client_token" ]; then
+	pid=$(pgrep get_client_token | awk 'NR==2 {print $1}')
+	echo "client at [$clientip] is not preauthenticated" | logger -p "daemon.warn" -s -t "NDS-Library[$pid]"
+	exit 1
+else
+	echo "$client_token"
+fi
+exit 0
---
+++ forward_authentication_service/libs/unescape.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Copyright (C) The Nodogsplash Contributors 2004-2020
+#Copyright (C) Blue Wave Projects and Services 2015-2020
+#This software is released under the GNU GPL license.
+
+option="$1"
+inputstr="$2"
+usage="
+  Usage: unescape.sh [-option] [escapedstring]
+
+  Returns: [unescapedstring]
+
+  Where:
+    [-option] is unescape type, currently -url only
+"
+
+if [ "$option" == "-url" ]; then
+	# shellcheck disable=SC2059  # we actually want to interpret the \xHH excape sequences
+	printf "${inputstr//%/\\x}"
+	exit 0
+fi
+
+if [ "$option" == "" ] || [ "$option" == "-h" ] || [ "$option" == "-help" ]; then
+	echo "$usage"
+	exit 0
+else
+	echo "Invalid option"
+	exit 1
+fi
