6c69d54b22ec35c4b0642127a6e78e4a31b488b9
Rob White <rob@blue-wave.net>
2020-03-03 14:28:23 +0000

    Set version to 4.5.0beta and include more style updates Preparing for v4.5.0 release
    Signed-off-by: Rob White <rob@blue-wave.net>

--- src/conf.h
+++ src/conf.h
@@ -29,20 +29,20 @@
 #ifndef _CONF_H_
 #define _CONF_H_
 
-#define VERSION "4.4.1beta"
+#define VERSION "4.5.0beta"
 
-/*@{*/
-/** Defines */
-/** How many times should we try detecting the interface with the default route
- * (in seconds).  If set to 0, it will keep retrying forever */
+/*
+ * Defines how many times should we try detecting the interface with the default route (in seconds).
+ * If set to 0, it will keep retrying forever.
+ */
 #define NUM_EXT_INTERFACE_DETECT_RETRY 0
-/** How long we should wait per try
- *  to detect the interface with the default route if it isn't up yet (interval in seconds) */
-#define EXT_INTERFACE_DETECT_RETRY_INTERVAL 1
-#define MAC_ALLOW 0 /** macmechanism to block MAC's unless allowed */
-#define MAC_BLOCK 1 /** macmechanism to allow MAC's unless blocked */
 
-/** Defaults configuration values */
+// How long we should wait per try to detect the interface with the default route if it isn't up yet (interval in seconds)
+#define EXT_INTERFACE_DETECT_RETRY_INTERVAL 1
+#define MAC_ALLOW 0 // macmechanism to block MAC's unless allowed
+#define MAC_BLOCK 1 // macmechanism to allow MAC's unless blocked
+
+// Defaults configuration values
 #ifndef SYSCONFDIR
 #define DEFAULT_CONFIGFILE "/etc/nodogsplash/nodogsplash.conf"
 #else
@@ -87,18 +87,16 @@
 #define DEFAULT_FW_MARK_BLOCKED 0x10000
 /* N.B.: default policies here must be ACCEPT, REJECT, or RETURN
  * In the .conf file, they must be allow, block, or passthrough
- * Mapping between these enforced by parse_empty_ruleset_policy() */
+ * Mapping between these enforced by parse_empty_ruleset_policy()
+ */
 #define DEFAULT_EMPTY_TRUSTED_USERS_POLICY "ACCEPT"
 #define DEFAULT_EMPTY_TRUSTED_USERS_TO_ROUTER_POLICY "ACCEPT"
 #define DEFAULT_EMPTY_USERS_TO_ROUTER_POLICY "REJECT"
 #define DEFAULT_EMPTY_AUTHENTICATED_USERS_POLICY "RETURN"
 #define DEFAULT_EMPTY_PREAUTHENTICATED_USERS_POLICY "REJECT"
 #define DEFAULT_IP6 0
-/*@}*/
 
-/**
-* Firewall targets
-*/
+// Firewall targets
 typedef enum {
 	TARGET_DROP,
 	TARGET_REJECT,
@@ -107,21 +105,17 @@ typedef enum {
 	TARGET_ULOG
 } t_firewall_target;
 
-/**
- * Firewall rules
- */
+// Firewall rules
 typedef struct _firewall_rule_t {
-	t_firewall_target target;	/**< @brief t_firewall_target */
-	char *protocol;		/**< @brief tcp, udp, etc ... */
-	char *port;			/**< @brief Port to block/allow */
-	char *mask;			/**< @brief Mask for the rule *destination* */
-	char *ipset;			/**< @brief IPset rule */
+	t_firewall_target target;	//@brief t_firewall_target
+	char *protocol;			//@brief tcp, udp, etc ...
+	char *port;			//@brief Port to block/allow
+	char *mask;			//@brief Mask for the rule *destination*
+	char *ipset;			//@brief IPset rule
 	struct _firewall_rule_t *next;
 } t_firewall_rule;
 
-/**
- * Firewall rulesets
- */
+// Firewall rulesets
 typedef struct _firewall_ruleset_t {
 	char *name;
 	char *emptyrulesetpolicy;
@@ -129,106 +123,102 @@ typedef struct _firewall_ruleset_t {
 	struct _firewall_ruleset_t *next;
 } t_firewall_ruleset;
 
-/**
- * MAC Addresses
- */
+// MAC Addresses
 typedef struct _MAC_t {
 	char *mac;
 	struct _MAC_t *next;
 } t_MAC;
 
 
-/**
- * Configuration structure
- */
+// Configuration structure
 typedef struct {
-	char configfile[255];		/**< @brief name of the config file */
-	char *ndsctl_sock;		/**< @brief ndsctl path to socket */
-	char *internal_sock;		/**< @brief internal path to socket */
-	int daemon;			/**< @brief if daemon > 0, use daemon mode */
-	int debuglevel;			/**< @brief Debug information verbosity */
-	int maxclients;			/**< @brief Maximum number of clients allowed */
-	char *gw_name;			/**< @brief Name of the gateway; e.g. its SSID or a unique identifier for use in a remote FAS */
-	char *http_encoded_gw_name;	/**< @brief http encoded name of the gateway, used as a templated variable in splash.htm */
-	char *url_encoded_gw_name;	/**< @brief url encoded name of the gateway used as variable in Preauth */
-	char *gw_interface;		/**< @brief Interface we will manage */
-	char *gw_iprange;		/**< @brief IP range on gw_interface we will manage */
-	char *gw_ip;			/**< @brief Internal IP (v4 or v6) for our web server */
-	char *gw_address;		/**< @brief Internal IP with port for our web server */
-	char *gw_mac;			/**< @brief MAC address of the interface we manage */
-	unsigned int gw_port;		/**< @brief Port the webserver will run on */
-	unsigned int fas_port;		/**< @brief Port the fas server will run on */
-	int login_option_enabled;	/**< @brief Use default PreAuth Login script  */
-	int use_outdated_mhd;		/**< @brief Use outdated libmicrohttpd  */
-	int unescape_callback_enabled;	/**< @brief Enable external MHD unescape callback script  */
-	int fas_secure_enabled;		/**< @brief Enable Secure FAS */
-	char *fas_path;			/**< @brief Path to forward authentication page of FAS */
-	char *fas_key;			/**< @brief AES key for FAS */
-	char *fas_remoteip;		/**< @brief IP addess of a remote FAS */
-	char *fas_remotefqdn;		/**< @brief FQDN of a remote FAS */
-	char *fas_url;			/**< @brief URL of a remote FAS */
-	char *fas_ssl;			/**< @brief SSL provider for FAS */
-	char *fas_hid;			/**< @brief Hash provider for FAS */
-	char *webroot;			/**< @brief Directory containing splash pages, etc. */
-	char *splashpage;		/**< @brief Name of main splash page */
-	char *statuspage;		/**< @brief Name of info status page */
-	char *redirectURL;		/**< @brief URL to direct client to after authentication */
-	char *authdir;			/**< @brief Notional relative dir for authentication URL */
-	char *denydir;			/**< @brief Notional relative dir for denial URL */
-	char *preauthdir;		/**< @brief Notional relative dir for preauth URL */
-	int session_timeout;		/**< @brief Minutes of the default session length */
-	int preauth_idle_timeout;	/**< @brief Minutes a preauthenticated client will be kept in the system */
-	int auth_idle_timeout;		/**< @brief Minutes an authenticated client will be kept in the system */
-	int checkinterval;		/**< @brief Period the the client timeout check thread will run, in seconds */
-	int set_mss;			/**< @brief boolean, whether to set mss */
-	int mss_value;			/**< @brief int, mss value; <= 0 clamp to pmtu */
-	int traffic_control;		/**< @brief boolean, whether to do tc */
-	int download_limit;		/**< @brief Download limit, kb/s */
-	int upload_limit;		/**< @brief Upload limit, kb/s */
-	int upload_ifb;			/**< @brief Number of IFB handling upload */
-	int log_syslog;			/**< @brief boolean, whether to log to syslog */
-	int syslog_facility;		/**< @brief facility to use when using syslog for logging */
-	int macmechanism; 		/**< @brief mechanism wrt MAC addrs */
-	t_firewall_ruleset *rulesets;	/**< @brief firewall rules */
-	t_MAC *trustedmaclist;		/**< @brief list of trusted macs */
-	t_MAC *blockedmaclist;		/**< @brief list of blocked macs */
-	t_MAC *allowedmaclist;		/**< @brief list of allowed macs */
-	unsigned int fw_mark_authenticated;	/**< @brief iptables mark for authenticated packets */
-	unsigned int fw_mark_blocked;	/**< @brief iptables mark for blocked packets */
-	unsigned int fw_mark_trusted;	/**< @brief iptables mark for trusted packets */
-	int ip6;			/**< @brief enable IPv6 */
-	char *binauth;			/**< @brief external authentication program */
-	char *preauth;			/**< @brief external preauthentication program */
+	char configfile[255];			//@brief name of the config file
+	char *ndsctl_sock;			//@brief ndsctl path to socket
+	char *internal_sock;			//@brief internal path to socket
+	int daemon;				//@brief if daemon > 0, use daemon mode
+	int debuglevel;				//@brief Debug information verbosity
+	int maxclients;				//@brief Maximum number of clients allowed
+	char *gw_name;				//@brief Name of the gateway; e.g. its SSID or a unique identifier for use in a remote FAS
+	char *http_encoded_gw_name;		//@brief http encoded name of the gateway, used as a templated variable in splash.htm
+	char *url_encoded_gw_name;		//@brief url encoded name of the gateway used as variable in Preauth
+	char *gw_interface;			//@brief Interface we will manage
+	char *gw_iprange;			//@brief IP range on gw_interface we will manage
+	char *gw_ip;				//@brief Internal IP (v4 or v6) for our web server
+	char *gw_address;			//@brief Internal IP with port for our web server
+	char *gw_mac;				//@brief MAC address of the interface we manage
+	unsigned int gw_port;			//@brief Port the webserver will run on
+	unsigned int fas_port;			//@brief Port the fas server will run on
+	int login_option_enabled;		//@brief Use default PreAuth Login script
+	int use_outdated_mhd;			//@brief Use outdated libmicrohttpd
+	int unescape_callback_enabled;		//@brief Enable external MHD unescape callback script
+	int fas_secure_enabled;			//@brief Enable Secure FAS
+	char *fas_path;				//@brief Path to forward authentication page of FAS
+	char *fas_key;				//@brief AES key for FAS
+	char *fas_remoteip;			//@brief IP addess of a remote FAS
+	char *fas_remotefqdn;			//@brief FQDN of a remote FAS
+	char *fas_url;				//@brief URL of a remote FAS
+	char *fas_ssl;				//@brief SSL provider for FAS
+	char *fas_hid;				//@brief Hash provider for FAS
+	char *webroot;				//@brief Directory containing splash pages, etc.
+	char *splashpage;			//@brief Name of main splash page
+	char *statuspage;			//@brief Name of info status page
+	char *redirectURL;			//@brief URL to direct client to after authentication
+	char *authdir;				//@brief Notional relative dir for authentication URL
+	char *denydir;				//@brief Notional relative dir for denial URL
+	char *preauthdir;			//@brief Notional relative dir for preauth URL
+	int session_timeout;			//@brief Minutes of the default session length
+	int preauth_idle_timeout;		//@brief Minutes a preauthenticated client will be kept in the system
+	int auth_idle_timeout;			//@brief Minutes an authenticated client will be kept in the system
+	int checkinterval;			//@brief Period the the client timeout check thread will run, in seconds
+	int set_mss;				//@brief boolean, whether to set mss
+	int mss_value;				//@brief int, mss value; <= 0 clamp to pmtu
+	int traffic_control;			//@brief boolean, whether to do tc
+	int download_limit;			//@brief Download limit, kb/s
+	int upload_limit;			//@brief Upload limit, kb/s
+	int upload_ifb;				//@brief Number of IFB handling upload
+	int log_syslog;				//@brief boolean, whether to log to syslog
+	int syslog_facility;			//@brief facility to use when using syslog for logging
+	int macmechanism; 			//@brief mechanism wrt MAC addrs
+	t_firewall_ruleset *rulesets;		//@brief firewall rules
+	t_MAC *trustedmaclist;			//@brief list of trusted macs
+	t_MAC *blockedmaclist;			//@brief list of blocked macs
+	t_MAC *allowedmaclist;			//@brief list of allowed macs
+	unsigned int fw_mark_authenticated;	//@brief iptables mark for authenticated packets
+	unsigned int fw_mark_blocked;		//@brief iptables mark for blocked packets
+	unsigned int fw_mark_trusted;		//@brief iptables mark for trusted packets
+	int ip6;				//@brief enable IPv6
+	char *binauth;				//@brief external authentication program
+	char *preauth;				//@brief external preauthentication program
 } s_config;
 
-/** @brief Get the current gateway configuration */
+// @brief Get the current gateway configuration
 s_config *config_get_config(void);
 
-/** @brief Initialise the conf system */
+// @brief Initialise the conf system
 void config_init(void);
 
-/** @brief Initialize the variables we override with the command line*/
+// @brief Initialize the variables we override with the command line
 void config_init_override(void);
 
-/** @brief Reads the configuration file */
+// @brief Reads the configuration file
 void config_read(const char filename[]);
 
-/** @brief Check that the configuration is valid */
+// @brief Check that the configuration is valid
 void config_validate(void);
 
-/** @brief Fetch a firewall rule list, given name of the ruleset. */
+// @brief Fetch a firewall rule list, given name of the ruleset. */
 t_firewall_rule *get_ruleset_list(const char[]);
 
-/** @brief Fetch a firewall ruleset, given its name. */
+// @brief Fetch a firewall ruleset, given its name.
 t_firewall_ruleset *get_ruleset(const char[]);
 
-/** @brief Add a firewall ruleset with the given name, and return it. */
+// @brief Add a firewall ruleset with the given name, and return it.
 t_firewall_ruleset *add_ruleset(const char[]);
 
-/** @brief Say if a named firewall ruleset is empty. */
+// @brief Say if a named firewall ruleset is empty.
 int is_empty_ruleset(const char[]);
 
-/** @brief Get a named empty firewall ruleset policy, given ruleset name. */
+// @brief Get a named empty firewall ruleset policy, given ruleset name.
 char * get_empty_ruleset_policy(const char[]);
 
 void parse_trusted_mac_list(const char[]);
@@ -251,7 +241,7 @@ int add_to_trusted_mac_list(const char possiblemac[]);
 int check_ip_format(const char[]);
 int check_mac_format(const char[]);
 
-/** config API, used in commandline.c */
+// config API, used in commandline.c
 int set_debuglevel(const char[]);
 
 #define LOCK_CONFIG() do { \
@@ -266,4 +256,4 @@ int set_debuglevel(const char[]);
 	debug(LOG_DEBUG, "Config unlocked"); \
 } while (0)
 
-#endif /* _CONF_H_ */
+#endif // _CONF_H_
