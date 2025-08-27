e5584909c7f27a6982dd152f1f36cfa74e9eb13f
Alexander Couzens <lynxis@fe80.eu>
2025-08-27 15:11:02 +0200

    path.h: add proper header/copyright/define
    Protect path.h from included multiple times and
    make it look like as other files of nodogsplash.

--- src/path.h
+++ src/path.h
@@ -1,2 +1,27 @@
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+\********************************************************************/
+
+/** @file path.h
+    @brief Path functions
+    @author Copyright (C) 2020 Alexander Couzens <lynxis@fe80.eu>
+*/
+
+#ifndef _NDS_PATH_
+#define _NDS_PATH_
 
 void buffer_path_simplify(char *dest, const char *src);
+
+#endif /* _NDS_PATH_ */
