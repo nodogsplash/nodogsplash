b59b8407000feb138c30eac783ae7b6e2060d81a
Arne Zachlod <arne@nerdkeller.org>
2025-11-27 19:31:52 +0100

    http_microhttpd.h: remove unused function prototype

--- src/http_microhttpd.h
+++ src/http_microhttpd.h
@@ -5,10 +5,6 @@
 
 struct MHD_Connection;
 
-/** @brief Get an IP's MAC address from the ARP cache.*/
-int arp_get(char mac_addr[18], const char req_ip[]);
-
-
 enum MHD_Result libmicrohttpd_cb (void *cls,
 					struct MHD_Connection *connection,
 					const char *url,
